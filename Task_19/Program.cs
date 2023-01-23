d97776b8634bccbf0d21dc10d400d59b2adec546
Dinara <ash.solarium@gmail.com>
2023-01-23 15:48:08 +0300

    Tasks added

---
+++ Task_19/Program.cs
@@ -0,0 +1,11 @@
+﻿// Task 19
+//  Хотела решить задачу через массив, но не могу понять, как написать корректный код
+
+Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [5];
+if (array[0] == array[4] && array[1] == array[2])
+    Console.WriteLine($"Число {n} полиндром");
+else
+    Console.WriteLine($"Число {n} не полиндром");
---
+++ Task_21/Program.cs
@@ -0,0 +1,22 @@
+﻿// Task 21
+
+Console.Clear();
+Console.Write("Введите координату точки a X: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату точки a Y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату точки a Z: ");
+double z = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату точки b X1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату точки b Y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату точки b Z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+double a = x1 - x;
+double b = y1 - y;
+double c = z1 - z;
+
+double length = Math.Sqrt(a * a + b * b + c * c);
+Console.WriteLine($"Длинна отрезка {length}");
